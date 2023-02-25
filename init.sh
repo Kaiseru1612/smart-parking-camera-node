df8f476c7f88cd7ea22dd1ba47eaa77dc61b7699
Kaiser1612 <vinhtien1612@gmail.com>
2023-02-26 02:18:32 +0700

    add init step

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod +x install-dependencies.sh wifi-connect.sh
+
+./install-dependencies.sh
---
+++ wifi-connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+network_name="Kaiser1612"
+password="1haiba47"
+
+nmcli device wifi connect "$network_name" password "$password"
